106f043db4f11d8a9170406c95bb28a8a3724a94
Nicholas Kearney <nakearney02@gmail.com>
2024-06-18 15:23:50 -0400

    Import Backend Libraries and Basic Structure

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,36 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+//External Package Imports
+const express = require('express');
+const {setGlobalOptions} = require("firebase-functions/v2");
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+//Project Utilities
+const {initializeSwaggerUI} = require('./utils/SwaggerWrapper');
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+//Project Service Account
+//Place it here after
+
+setGlobalOptions({ maxInstances: 10 });
+
+//Instantiate express.js, firebase instance & service account
+const app = express();
+// const admin = getAdmin();
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
 // });
+
+//Instantiate API route classes AFTER express.js is created 
+// const proxyHello = require("./proxy/hello");
+// const v1Hello = require("./backend/hello");
+
+//Define the route paths and their corresponding definitions
+// app.use('/proxy/hello', proxyHello);
+// app.use('/v1/hello', v1Hello);
+
+//Create Swagger UI page for the provided API application
+initializeSwaggerUI(app, '/docs', ["./proxy/*.js", "./backend/*.js"]);
+
+//Export API to HTTPS server
+exports.api = onRequest(app);
